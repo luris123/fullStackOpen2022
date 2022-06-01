f8937199f413b8ad095d9114f98f43fc28e88c61
luris123 <anssi.rauhala1@gmail.com>
2022-06-01 16:53:45 +0300

    2.8 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456'  }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+    if (persons.some(e => e.name === newName)) {
+      alert(newName + ' is already added to phonebook')
+    } 
+    else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>filter shown with <input></input></div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
+        <div>name: <input value={newName} onChange={handleNameChange} /></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
+        <div><button type="submit">add</button></div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>{persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}</ul>
+    </div>
+  )
+
+}
+
+export default App
