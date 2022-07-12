a20db47cb3b308fed51c4f7ca36aefeedec9d905
luris123 <anssi.rauhala1@gmail.com>
2022-07-12 23:34:25 +0300

    5.7 done

--- part5/bloglist-backend/controllers/blogs.js
+++ part5/bloglist-backend/controllers/blogs.js
@@ -4,6 +4,14 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 router.get('/', async (request, response) => {
   const notes = await Blog
     .find({})
@@ -13,19 +21,34 @@ router.get('/', async (request, response) => {
 })
 
 router.post('/', async (request, response) => {
-  if (!request.user) {
+  const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
+  const user = await User.findById(decodedToken.id)
 
-  const user = request.user
-  const blog = new Blog({ ...request.body, user: user.id })
+    if(!body.title || !body.url) {
+      return response.status(400).json({ error: 'title and url are required' })
+    }
 
-  const savedBlog = await blog.save()
+    if (!body.likes) {
+        body.likes = 0
+    }
 
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
 
-  response.status(201).json(savedBlog)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+    response.json(savedBlog)
 })
 
 router.delete('/:id', async (request, response) => {
--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,53 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import blogService from '../services/blogs'
 
+const Blog = ({blog, setBlogs}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [visible, setVisible] = useState(false)
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const handleLike = async (event) => {
+    event.preventDefault()
+
+    const blogObject = {
+      user: blog.user.id,
+      likes: blog.likes + 1,
+      author: blog.author,
+      title: blog.title,
+      url: blog.url
+  }
+    await blogService.update(blog.id, blogObject)
+    const updatedBlogs = await blogService.getAll()
+    setBlogs(updatedBlogs)
+  }
+
+
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <b>by</b> {blog.author} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      <div>
+          {visible ? <><b>url:</b> {blog.url}</> : null}
+      </div>
+      <div>
+          {visible ? <><b>likes:</b> {blog.likes}</> : null} {visible ? <button onClick={handleLike}>like</button> : null}
+      </div>
+      <div>
+         {visible ? <><b>user:</b> {blog.user.username}</> : null}
+      </div>
+
+      
+    </div>
+  )
+}
 export default Blog
